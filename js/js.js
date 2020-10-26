ce10fdbc5b33407072307c18264f97fc8a92d155
Fabo <40230@pm.me>
2020-10-26 13:17:18 -0500

    sdf

--- js/js.js
+++ js/js.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 	$("bio").hide();  
 	$("name").click(function(){
-	 		$(".sketchfab-embed-wrapper").hide();  
+	 		$("bio").show();
+	 		$("bio").css("top","9vh");
+		}
+	);
+
+	$("#bio").click(function(){
 	 		$("bio").show();
 	 		$("bio").css("top","9vh");
 		}
