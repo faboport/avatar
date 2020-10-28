8b92e7f41842c25aef3d2ea8929b206ab9e672ec
fabohax <40230@pm.me>
2020-10-27 20:55:40 -0500

    .

--- js/js.js
+++ js/js.js
@@ -2,13 +2,11 @@ $(document).ready(function(){
 	$("bio").hide();  
 	$("name").click(function(){
 	 		$("bio").show();
-	 		$("bio").css("top","9vh");
 		}
 	);
 
 	$("#bio").click(function(){
 	 		$("bio").show();
-	 		$("bio").css("top","9vh");
 		}
 	);
 
